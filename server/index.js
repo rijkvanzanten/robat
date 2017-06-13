825efc2df42b4a523426285e117fa5f1aa40f004
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-13 16:35:33 +0200

    :fire: remove post route support

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
-const postHandler = require('./post-handler');
 const socketHandler = require('./socket-handler');
 
 require('dotenv').config();
@@ -22,8 +21,7 @@ const app = express()
   .set('view engine', 'ejs')
   .use(express.static(path.join(__dirname, '..', 'static'), {maxAge: '31d'}))
   .use(express.static(path.join(__dirname, '..', 'build'), {maxAge: '31d'}))
-  .get('*', renderEmptyIndex)
-  .post('*', postHandler);
+  .get('*', renderEmptyIndex);
 
 // Start server on provided port or other 3000
 const server = http.createServer(app);
@@ -32,12 +30,11 @@ const io = socketIO(server);
 io.on('connection', socketHandler);
 
 const port = process.env.PORT || 3000;
+
 server.listen(port, () => {
   console.log('🤖  Robat is listening at port '+ port); // eslint-disable-line no-console
 });
 
 function renderEmptyIndex(req, res) {
-  res.render('index', {
-    messages: [],
-  });
+  res.render('index');
 }
