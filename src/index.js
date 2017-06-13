23d459023b01436571489368778299303db94cbe
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-13 11:35:44 +0200

    :sparkles: render message to dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 (function () {
   const socket = io.connect();
 
+  const chatWindow = document.querySelector('ul');
+
   document.querySelector('form').addEventListener('submit', submitMessage);
 
   /**
@@ -14,13 +16,19 @@
     // sockets
     socket.emit('message', message);
 
+    renderMessage(message);
+
     messageForm.querySelector('input[name="message"]').value = '';
     event.preventDefault();
   }
 
   function scrollMessages() {
-    const messagesElement = document.querySelector('ul');
-    messagesElement.scrollTop = messagesElement.scrollHeight;
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+  }
+
+  function renderMessage(message, robat = false) {
+    chatWindow.innerHTML += `<li data-user="${robat ? 'robat' : 'user'}">${message}</li>`;
+    scrollMessages();
   }
 
   // Scroll to most bottom message on load
