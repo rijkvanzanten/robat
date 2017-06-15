4ec5aca4e9e3ff9c62a2add1563d290b13206b1b
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-15 23:15:28 +0200

    :sparkles: re-add firstEntityValue function

--- server/utils.js
+++ server/utils.js
@@ -1,4 +1,16 @@
 module.exports = {
+  firstEntityValue(entities, entity) {
+    const val = entities && entities[entity] &&
+      Array.isArray(entities[entity]) &&
+      entities[entity].length > 0 &&
+      entities[entity][0].value;
+
+    if (!val) {
+      return null;
+    }
+
+    return typeof val === 'object' ? val.value : val;
+  },
   translateType(val) {
     val = val.toLowerCase();
     // TODO: complete list
