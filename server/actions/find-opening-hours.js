46e2b85292ef57ee590bf225277a4e21df902507
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-15 23:13:18 +0200

    :sparkles: add find-price action (WIP)

--- server/actions/find-opening-hours.js
+++ server/actions/find-opening-hours.js
@@ -1,5 +1,19 @@
 const actions = require('../actions');
+const {firstEntityValue} = require('../utils');
 
-actions.register('findOpeningHours', function() {
-  return '10 - 15u';
-});
+actions.register('findOpeningHours', findOpeningHours);
+
+function findOpeningHours({context, entities}) {
+  const location = firstEntityValue(entities, 'obaLocation');
+
+  if (location) {
+    context.location = location;
+    context.openingHours = '10:00 - 15:00';
+    delete context.missingLocation;
+  } else {
+    context.missingLocation = true;
+    delete context.location;
+    delete context.openingHours;
+  }
+  return context;
+}
