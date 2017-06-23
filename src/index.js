5bc5d25efd9588c1369be165b7513fe2de70475c
dandevri <vriesdan@gmail.com>
2017-06-23 14:10:10 +0200

    :construction: Start refactoring front-end code

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@
 const shortid = require('shortid');
 const localforage = require('localforage');
 
-let messageList = [];
-const chatWindow = document.querySelector('ul');
+// Register service worker
+if ('serviceWorker' in navigator) {
+  // Where to find the service worker
+  navigator.serviceWorker.register('/sw.js');
+}
 
 localforage.getItem('chatMessages', init);
 
 function init(err, data) {
-  if (data && data.messageList) {
-    messageList = data.messageList;
-    data.messageList.forEach(message => renderMessage(message, message.id === 0, true));
-  }
-
   const socket = io.connect();
 
   // Check for online and offline events
@@ -21,118 +19,87 @@ function init(err, data) {
 
   document.querySelector('form').addEventListener('submit', submitMessage);
 
-  socket.on('message', serverMessage);
-  socket.on('messageReceived', addReceived);
-  socket.on('displayResults', renderResults);
-
-  function updateStatus() {
-    // Select the indicator element
-    if(navigator.onLine) {
-      document.querySelector('[data-tooltip]').setAttribute('data-tooltip', 'online');
-    } else {
-      document.querySelector('[data-tooltip]').setAttribute('data-tooltip', 'offline');
-    }
-  }
-
-
-  // Set the messageList array in localstorage
-  function setLocalStorage(message) {
-    messageList.push(message);
-    localforage.setItem('chatMessages', {
-      messageList: messageList,
-    });
-  }
-
-  function serverMessage(message) {
-    updateStatus();
-    message = {
-      value: message,
-      id: 0,
-    };
-
-    renderMessage(message, true);
-    document.querySelector('#loader').classList.add('hide');
-    setLocalStorage(message);
-    scrollMessages();
-  }
-
-  /**
-   * Reads the value of the input and sends it to renderMessageToDom
-   */
-  function submitMessage(event) {
-    updateStatus();
-    const messageForm = document.querySelector('form');
-    const message = {
-      value: messageForm.querySelector('input[name="message"]').value,
-      id: shortid.generate(),
-    };
-
-    if (message.value.length > 0) {
-      // sockets
-      socket.emit('message', message);
-      setLocalStorage(message);
-
-      renderMessage(message);
-
-      messageForm.querySelector('input[name="message"]').value = '';
-    }
-
-    event.preventDefault();
-  }
-
-  /**
-   * Renders a new message to the chatwindow
-   * @param  {String} message The message to render
-   * @param  {Boolean} robat Is this a message of robat
-   */
-  function renderMessage(message, robat = false, fromStorage = false) {
-    chatWindow.innerHTML += `<li class="${fromStorage && !robat ? 'received' : ''}" data-id="${message.id}" data-user="${robat ? 'robat' : 'user'}">${message.value}</li>`;
-    scrollMessages();
-  }
-
-  function addReceived({id}) {
-    document.querySelector('[data-id="' + id + '"]').classList.add('received');
-    document.querySelector('#loader').classList.remove('hide');
-    scrollMessages();
-  }
-
-  function renderResults(results) {
-    if (results && results.length > 0) {
-      return renderMessage(`
-        <ul>
-          ${results.map(renderLI).reduce((str, item) => str += item)}
-          <button>Toon meer</button>
-        </ul>
-      `, true);
-    }
-
-    function renderLI(item) {
-      const {title, author, image} = item;
-      return `
-        <li>
-          <div>
-            <h2>${title}</h2>
-            <h3>${author}</h3>
-          </div>
-          <img src="${image}" alt="${title}" />
-        </li>
-      `;
-    }
-  }
-
-  /**
-   * Scrolls the message window to the last item
-   */
-  function scrollMessages() {
-    chatWindow.parentNode.scrollTop = chatWindow.parentNode.scrollHeight;
-  }
+  socket.on('message', onReceiveMessageFromServer);
+  socket.on('messageReceived', addReceivedStatusToMessage);
 
   // Scroll to most bottom message on load
   scrollMessages();
+}
 
-  // Register service worker
-  if ('serviceWorker' in navigator) {
-    // Where to find the service worker
-    navigator.serviceWorker.register('/sw.js');
+/**
+ * Fires when we receive a message from the server
+ * @param  {Object} message Message object
+ */
+function onReceiveMessageFromServer(message) {
+  updateStatus();
+
+  renderMessage(message, true);
+  document.querySelector('#loader').classList.add('hide');
+  scrollMessages();
+}
+
+/**
+ * Add received icon to specific message
+ * @param {String} id ID of message to add icon to
+ */
+function addReceivedStatusToMessage(id) {
+  document.querySelector('[data-id="' + id + '"]').classList.add('received');
+  document.querySelector('#loader').classList.remove('hide');
+  scrollMessages();
+}
+
+/**
+ * Send message to server
+ * @param  {Object} event Form submit event
+ * @param  {Object} socket Current connected socket
+ */
+function submitMessage(event, socket) {
+  updateStatus();
+  const messageForm = document.querySelector('form');
+  const message = {
+    value: messageForm.querySelector('input[name="message"]').value,
+    id: shortid.generate(),
+  };
+
+  if (message.value.length > 0) {
+    // sockets
+    socket.emit('message', message);
+
+    renderMessage(message);
+
+    messageForm.querySelector('input[name="message"]').value = '';
   }
+
+  event.preventDefault();
+}
+
+/**
+ * Update the online indicator
+ */
+function updateStatus() {
+  // Select the indicator element
+  const indicator = document.querySelector('[data-tooltip]');
+  if(navigator.onLine) {
+    indicator.setAttribute('data-tooltip', 'online');
+  } else {
+    indicator.setAttribute('data-tooltip', 'offline');
+  }
+}
+
+/**
+ * Scrolls the message window to the last item
+ */
+function scrollMessages() {
+  const chatWindow = document.getElementById('messages');
+  chatWindow.parentNode.scrollTop = chatWindow.parentNode.scrollHeight;
+}
+
+/**
+ * Renders a new message to the chatwindow
+ * @param  {String} message The message to render
+ * @param  {Boolean} robat Is this a message of robat
+ */
+function renderMessage(message, robat = false, fromStorage = false) {
+  // chatWindow.innerHTML += `<li class="${fromStorage && !robat ? 'received' : ''}" data-id="${message.id}" data-user="${robat ? 'robat' : 'user'}">${message.value}</li>`;
+  // scrollMessages();
 }
