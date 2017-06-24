9f75e85b642b4fea5b2e93c1cea351e1577a3c55
GiuliaM <Gekke_giulia@hotmail.com>
2017-06-24 18:09:27 +0200

    :construction: Making facets dynamically

--- server/actions/search.js
+++ server/actions/search.js
@@ -10,4 +10,47 @@ function search({context, entities}) {
   const language = firstEntityValue(entities, 'language');
   const year = firstEntityValue(entities, 'year');
   const searchQuery = firstEntityValue(entities, 'search_query');
+  const parameters = {};
+
+  if (type) {
+    if (parameters.facet) {
+      parameters.facet.push(`Type(${type})`);
+    } else {
+      parameters.facet = [`Type(${type})`];
+    }
+  }
+
+  if (language) {
+    if (parameters.facet) {
+      parameters.facet.push(`Language(${language})`);
+    } else {
+      parameters.facet = [`Language(${language})`];
+    }
+  }
+
+  if (year) {
+    if (parameters.facet) {
+      parameters.facet.push(`pubYear(${language})`);
+    } else {
+      parameters.facet = [`pubYear(${language})`];
+    }
+  }
+
+  return OBAClient.get('search', parameters
+  )
+    .then(res => JSON.parse(res))
+    .then(res => {
+      const count = res.aquabrowser.meta.count;
+
+      if (count > 0) {
+        context.title = res.aquabrowser.results.result.titles['short-title'];
+      } else {
+        context.notFound = true;
+      }
+
+      return context;
+
+    })
+    .catch(err => console.log(err)); // eslint-disable-line no-console
+
 }
